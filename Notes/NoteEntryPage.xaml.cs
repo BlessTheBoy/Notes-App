1bf6d20cee3b06e151fcfa678a5e110783422d1a
Akorede Bakare <bakarefaruq.a@gmail.com>
2019-12-25 13:38:54 +0100

    Made it a multipage app

---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using Notes.Models;
+
+namespace Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.FileName))
+            {
+                //save
+                var fileName = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(fileName, note.Text);
+            }
+            else
+            {
+                File.WriteAllText(note.FileName, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (File.Exists(note.FileName))
+            {
+                File.Delete(note.FileName);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+}
